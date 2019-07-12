55bc231676e043f7eb0308f254fc04b55e8aad44
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-12 07:14:40 -0400

    completed about us comp

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const AboutUs = () => {
+    const imgLink = "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/651334/600/400/m2/fpnw/wm0/atlanta03small-.jpg?1441996098&s=583b356665fedb0f12a8701b9c7df4fe";
+    
     return (
         <section className="section-container" id="aboutUs">
             <div className="pages-background-image">
                 <h1>Your Deams of Owning A Home Can Come True</h1>
-
-                <img className="aboutus-img" src="https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/651334/600/400/m2/fpnw/wm0/atlanta03small-.jpg?1441996098&s=583b356665fedb0f12a8701b9c7df4fe"/>
-            </div>
-                <br/>
-            <article>
+                <article>
                 <h2>About Atlanta RealEstate.com®</h2>
                 <br></br>
                    <main>
@@ -21,10 +19,10 @@ const AboutUs = () => {
                         owners to manage their home like the important investment it is by tracking their home’s value over time, researching and managing home improvements, 
                         and scouting other similar properties in the neighborhood.
                    </main>
-
-
             </article>
 
+                <img src={imgLink} alt=""/>
+            </div>
         </section>
     )
 }
