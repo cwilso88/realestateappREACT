9f1e3513ae97b7501e67e69c03626b897796ab87
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-12 02:56:56 -0400

    Transferring more files to the create react app

---
+++ src/components/pages/Advertise.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Advertise extends Component {
+    render() {
+        return (
+            <div>
+                Advertise
+            </div>
+        )
+    }
+}
+
+export default Advertise;
