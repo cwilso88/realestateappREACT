ae441a32603ae5f6223db03816f700bd10975a81
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-12 03:22:55 -0400

    Fixing a few import statements for to reflect the change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import RealEstate from './RealEstate';
-import Header from './Header';
-import AboutUs from './pages/AboutUs'
-import Login from './pages/Login'
-import Advertise from './pages/Advertise'
-import Register from './pages/Register'
-import RealEstate from './RealEstate';
+import RealEstate from './components/layout/RealEstate';
+import Header from './components/layout/Header';
+import AboutUs from './components/pages/AboutUs';
+import Login from './components/pages/Login';
+import Advertise from './components/pages/Advertise';
+import Register from './components/pages/Register';
+
 
 
 class App extends Component {
