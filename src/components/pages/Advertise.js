2bb46acd12a236032c1f0dc0a4b1e2b0139d7028
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-12 07:35:22 -0400

    Updated the main image for the advertise comp

--- src/components/pages/Advertise.js
+++ src/components/pages/Advertise.js
@@ -1,28 +1,24 @@
 import React from 'react';
 
 const Advertise = () => {
-    const imgLink = "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/651334/600/400/m2/fpnw/wm0/atlanta03small-.jpg?1441996098&s=583b356665fedb0f12a8701b9c7df4fe";
+    const imgLink = "https://www.lemoptix.com/wp-content/uploads/ngg_featured/Are-You-Suitable-for-a-Career-in-Real-Estate-Picture-1200x480.jpg";
     
     return (
         <section className="section-container" id="advertise">
             <div className="pages-background-image">
-                <h1>Your Deams of Owning A Home Can Come True</h1>
-                <article>
+                <h1>Sell Your Home Quickly</h1>
+
+                <img src={imgLink} alt="Buy a home in Atlanta"/>
+            </div>
+            <article>
                 <h2>About Atlanta RealEstate.com®</h2>
                 <br></br>
                    <main>
-                            For years, millions of home shoppers have turned to us to find their dream home. We offer a 
-                        comprehensive list of for-sale properties, as well as the information and tools to make informed real estate decisions. Today, more than ever, 
-                        Atlanta Real Estate is The Home of Home Search℠.
-
                         Atlanta Real Estate also offers homeowners a bevy of useful tools and resources through the My Home℠ dashboard. My Home℠ dashboard allows property 
                         owners to manage their home like the important investment it is by tracking their home’s value over time, researching and managing home improvements, 
                         and scouting other similar properties in the neighborhood.
                    </main>
             </article>
-
-                <img src={imgLink} alt="Buy a home in Atlanta"/>
-            </div>
         </section>
     )
 }
