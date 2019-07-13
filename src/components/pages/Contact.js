46a390d1dbe112fffba667c56dcef0edf72d5cf0
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-13 10:34:07 -0400

    moved the margin for the form to be handled in the scss style sheet

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,7 +6,7 @@ class Login extends Component {
         return (
             <div className="ui middle aligned center aligned grid content-area" id="contact" style={{ backgroundColor: "#dadde2" }}>
                 <div className="ui container">			
-			        <form className="ui form pad-top-30 pad-bottom-30" style={{ margin: "10%" }}>
+			        <form className="ui form pad-top-30 pad-bottom-30">
 			            <h3 className="huge header inverted">Contact Us</h3>
                         <div className="two fields">
                             <div className="field">
@@ -21,18 +21,6 @@ class Login extends Component {
                         <div className="field">
                             <label>Leave your message here:</label>
                             <textarea></textarea>
-                        </div>
-                        <div class="field">
-                            <label>Message Type:</label>
-                            <div className="ui selection dropdown">
-                                <input type="hidden" name="message-type" />
-                                <i className="dropdown icon"></i>
-                                <div className="default text">Message Type</div>
-                                <div className="menu">
-                                    <div className="item" data-value="1">Complaint</div>
-                                    <div className="item" data-value="0">Query</div>
-                                </div>
-                            </div>
                         </div>
                         <div className="field">
                             <label>Email</label>
