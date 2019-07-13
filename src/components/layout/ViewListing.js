8e723f60f7459f1155d1bd578c9ff38426479d5b
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-13 14:15:03 -0400

    Tried to convert the ViewListing comp to a func based comp in hopes to match param id, but that did not work

--- src/components/layout/ViewListing.js
+++ src/components/layout/ViewListing.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import listingsData from '../../data/listingsData';
 
-class ViewListing extends Component {
-    state = {
-        listingsData
-    }
-    
-    render() {
-        var listingsData = this.state.listingsData;
-        console.log(listingsData);
-        
+const ViewListing = (props) => { 
         return (
+        const listing = listingsData;
+        const id = this.props.match.params.id;
+        console.log(listing[id]);
+        console.log(this.props.match.params.id);
             <div>
                 ViewListing
                 
             </div>
         )
-    }
+    
 }
 
 export default ViewListing;
