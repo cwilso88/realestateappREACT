2b4135d853ee0d717184359623658201ae754946
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-14 07:36:40 -0400

    Switched out the semantic ui nested segments for the a grid system

--- src/components/layout/ViewListing.js
+++ src/components/layout/ViewListing.js
@@ -8,7 +8,7 @@ const ViewListing = (props) => {
         console.log(currentListing);
 
         return ( 
-            <div id="viewListing" className="content-area">
+            <div id="content-area viewListing">
                 <div className="ui padded segment">
                     <img className="ui fluid image" alt="house" src="https://i.kinja-img.com/gawker-media/image/upload/s--bIV3xkEm--/c_scale,f_auto,fl_progressive,q_80,w_800/jsprifdd1gmfy7e7nola.jpg"></img>
                     <button class="ui button">
@@ -16,6 +16,16 @@ const ViewListing = (props) => {
                         Click to expand photo
                     </button>
                 </div>
+
+                <div class="ui equal width grid">
+                    <div class="column"></div>
+                    <div class="column"></div>
+                    <div class="column"></div>
+                    <div class="equal width row">
+                        <div class="column"></div>
+                        <div class="column"></div>
+                    </div>
+                </div>      
             </div>
         )
 }
