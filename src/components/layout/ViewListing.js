1af43995a8c637b7aaa701b17ecb37693cd3e632
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-14 07:23:50 -0400

    Adding styling to the ViewListing comp

--- src/components/layout/ViewListing.js
+++ src/components/layout/ViewListing.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import listingsData from '../../data/listingsData';
 
 const ViewListing = (props) => { 
-        return (
         const listing = listingsData;
-        const id = this.props.match.params.id;
-        console.log(listing[id]);
-        console.log(this.props.match.params.id);
-            <div>
-                ViewListing
-                
+        const id = props.match.params.id;
+        const currentListing = listing[id];
+        console.log(currentListing);
+
+        return ( 
+            <div id="viewListing" className="content-area">
+                <div className="ui padded segment">
+                    <img className="ui fluid image" alt="house" src="https://i.kinja-img.com/gawker-media/image/upload/s--bIV3xkEm--/c_scale,f_auto,fl_progressive,q_80,w_800/jsprifdd1gmfy7e7nola.jpg"></img>
+                    <button class="ui button">
+                        <i class="icon camera"></i>
+                        Click to expand photo
+                    </button>
+                </div>
             </div>
         )
-    
 }
 
 export default ViewListing;
